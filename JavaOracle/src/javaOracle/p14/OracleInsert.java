a973639676c652106eb2dc91ede78d41cce6e30b
Administrator <Administrator@User-2022YIVWQM>
2022-12-27 18:10:46 +0900

    dfdf

--- JavaOracle/src/javaOracle/p14/OracleInsert.java
+++ JavaOracle/src/javaOracle/p14/OracleInsert.java
@@ -7,8 +7,8 @@ public class OracleInsert {
 	public static void main(String[] args) {
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "c##green";
-		String password = "green1234";
+		String user = "c##nanovia3";
+		String password = "ehdrn1";
 		try {
 			Class.forName(driver);
 			System.out.println("jdbc driver loading success.");
@@ -16,31 +16,32 @@ public class OracleInsert {
 			System.out.println("oracle connection succexx.\n");
 			Statement stmt = conn.createStatement();
 
-			String sql3 = "SELECT max(deptno) from dept";
+			String sql3 = "SELECT yadmnm from covid";
 			ResultSet rs2 = stmt.executeQuery(sql3);
 
-			rs2.next();
-			int n = rs2.getInt("max(deptno)") + 10;
-			String str = Integer.toString(n);
-			String ideptno = str, sdname = "IT", sloc = "SEOUL";
-			String sql = "INSERT INTO dept VALUES ('" + ideptno + "','" + sdname + "','" + sloc + "')";
-			boolean b = stmt.execute(sql);
+//			rs2.next();
+//			int n = rs2.getInt("max(deptno)") + 10;
+//			String str = Integer.toString(n);
+//			String ideptno = str, sdname = "IT", sloc = "SEOUL";
+//			String sql = "INSERT INTO dept VALUES ('" + ideptno + "','" + sdname + "','" + sloc + "')";
+//			boolean b = stmt.execute(sql);
 
-			if (!b) {
-				System.out.println("Insert success. \n");
-			} else {
-				System.out.println("Insert fail.\n");
-			}
-			// String sql = "SELECT * FROM dept";
+//			if (!b) {
+//				System.out.println("Insert success. \n");
+//			} else {
+//				System.out.println("Insert fail.\n");
+//			}
+//			// String sql = "SELECT * FROM dept";
 
-			String sql2 = "SELECT deptno, dname, loc FROM dept order by deptno";
+			String sql2 = "SELECT sidonm, sggunm, yadmnm , telno FROM covid";
 
 			ResultSet rs = stmt.executeQuery(sql2);
 
 			while (rs.next()) {
-				System.out.print(rs.getString("deptno") + "\t");
-				System.out.print(rs.getString("dname") + "\t");
-				System.out.println(rs.getString("loc") + " ");
+				System.out.print(rs.getString("sidonm") + "\t");
+				System.out.print(rs.getString("sggunm") + "\t");
+				System.out.print(rs.getString("telno") + "\t");
+				System.out.println(rs.getString("yadmnm") + " ");
 			}
 		} catch (ClassNotFoundException e) {
 			System.out.println(e);
